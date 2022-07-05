ccbc5e29e96f83dd4f4676d33d2c481fa63a5ffa
M Tarek <mohiuddintarek2017@gmail.com>
2022-07-05 15:20:50 +0600

    first commit

---
+++ resources/views/admin/addpost.blade.php
@@ -0,0 +1,26 @@
+@include('admin/header')
+<div class="container">
+
+<div class="row">
+    <div class="col-6">
+        <h1>Image</h1>
+    </div>
+    <div class="col-6 card p-5 m-3">
+    <form action="" method="post" enctype="">
+    <!-- Name input -->
+    <div class="form-outline mb-3">
+        <input name="title" type="text" id="form4Example1" class="form-control" />
+        <label class="form-label" for="form4Example1">Title</label>
+    </div>
+
+    <!-- Email input -->
+    <div class="form-outline mb-3">
+        <input type="file" id="form4Example2" class="form-control" />
+        <label class="form-label" for="form4Example2">Post Image</label>
+    </div>
+    <button type="submit" class="btn btn-primary btn-block mb-4">Save</button>
+    </form>
+    </div>
+    </div>
+    </div>
+@include('admin/footer')
---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,35 @@
+<section class="footer" id="contact">
+        <div class="box-container">
+            <div class="box">
+                <h3>about us</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi vitae doloremque beatae? Mollitia, culpa repellendus eaque tempore optio voluptatibus officiis quod facilis quidem!</p>
+            </div>
+            <div class="box">
+                <h3>branch location</h3>
+                <a href="#">india</a>
+                <a href="#">USA</a>
+                <a href="#">Japan</a>
+                <a href="#">France</a>
+            </div>
+            <div class="box">
+                <h3>Quick Links</h3>
+                <a href="#home">Home</a>
+                <a href="#book">Book</a>
+                <a href="#packages">Packages</a>
+                <a href="#services">Services</a>
+                <a href="#gallery">Gallery</a>
+                <a href="#review">Review</a>
+                <a href="#contact">Contact</a>
+            </div>
+            <div class="box">
+                <h3>Follow Us</h3>
+                <a href="#"><i class="fa-brands fa-facebook-f"></i> facebook</a>
+                <a href="#"><i class="fa-brands fa-instagram"></i> instagram</a>
+                <a href="#"><i class="fa-brands fa-twitter"></i> twitter</a>
+                <a href="#"><i class="fa-brands fa-linkedin-in"></i> linkedin</a>
+            </div>
+        </div>
+        <h1 class="credit">&copy 2017 - 2022 | Developed By <br> <span>Dhaka Digital</span> </h1>
+    </section>
+</body>
+</html>
